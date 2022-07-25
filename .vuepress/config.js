e2ac85800b96bfb71c291b2be812c55c98f3bc22
lvyou03 <lvyou03@corp.netease.com>
2022-07-25 14:41:50 +0800

    [update] update blog

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: "Felikslv的博客",
+  title: "FeliksLv的博客",
   description: "记录学习过程，分享生活琐碎。",
   dest: "public",
   locales: {
@@ -71,7 +71,7 @@ module.exports = {
     search: true,
     searchMaxSuggestions: 10,
     lastUpdated: "Last Updated",
-    author: "Felikslv",
+    author: "FeliksLv",
     authorAvatar: "/avatar.png",
     startYear: "2019"
   },
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ---
 home: true
-heroText: Felikslv的博客
+heroText: FeliksLv的博客
 tagline: 记录学习过程，分享生活琐碎。
 bgImageStyle: { height: '450px' }
 isShowTitleInHome: false
